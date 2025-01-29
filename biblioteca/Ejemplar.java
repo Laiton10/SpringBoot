dc5213bd2fa467d6e662ec0d57b724d9220b9c93
Laiton10 <161719246+Laiton10@users.noreply.github.com>
2025-01-29 20:14:15 +0100

    Add files via upload

---
+++ biblioteca/Ejemplar.java
@@ -0,0 +1,49 @@
+package com.example.demo.biblioteca;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Table(name = "ejemplar")
+public class Ejemplar {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "isbn", nullable = false)
+    private com.example.demo.biblioteca.Libro isbn;
+
+    @ColumnDefault("'Disponible'")
+    @Lob
+    @Column(name = "estado")
+    private String estado;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public com.example.demo.biblioteca.Libro getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(com.example.demo.biblioteca.Libro isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+}
---
+++ biblioteca/Prestamo.java
@@ -0,0 +1,72 @@
+package com.example.demo.biblioteca;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "prestamo")
+public class Prestamo {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private com.example.demo.biblioteca.Usuario usuario;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "ejemplar_id", nullable = false)
+    private Ejemplar ejemplar;
+
+    @Column(name = "fechaInicio", nullable = false)
+    private LocalDate fechaInicio;
+
+    @Column(name = "fechaDevolucion")
+    private LocalDate fechaDevolucion;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public com.example.demo.biblioteca.Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(com.example.demo.biblioteca.Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Ejemplar getEjemplar() {
+        return ejemplar;
+    }
+
+    public void setEjemplar(Ejemplar ejemplar) {
+        this.ejemplar = ejemplar;
+    }
+
+    public LocalDate getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(LocalDate fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public LocalDate getFechaDevolucion() {
+        return fechaDevolucion;
+    }
+
+    public void setFechaDevolucion(LocalDate fechaDevolucion) {
+        this.fechaDevolucion = fechaDevolucion;
+    }
+
+}
